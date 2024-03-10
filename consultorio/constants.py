9669bf05523f7d0e8c5051f55e4cc5f2036ab782
Oscar Enrique Gonzalez Escurra <ogonzalez@roshka.com>
2024-03-10 17:28:57 -0300

    se agrega odontologia a los usuarios

--- consultorio/constants.py
+++ consultorio/constants.py
@@ -6,6 +6,7 @@ SEX = (
 TIPOS_USERS = (
     ('nutricion', 'Nutrición'),
     ('psicologia', 'Psicología'),
+    ('odontologia', 'Odontología'),
     ('psiquiatria', 'Psiquiatría'),
     ('dematologia', 'Dermatología'),
     ('kinesiologia', 'Kinesiología'),
